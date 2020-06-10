64cd26fc155b9242d0449b34b7ca9959424085fb
Yonghyun <lygon55555@gmail.com>
2020-06-10 15:36:49 +0900

    base setting

---
+++ SolKeeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SolKeeper
+//
+//  Created by Yonghyun on 2020/06/10.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
